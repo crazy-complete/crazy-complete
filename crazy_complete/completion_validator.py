ee50f0c4bb9569729e95a4cc2e5b64d9b3d8fa83
Benjamin Abendroth <braph93@gmx.de>
2025-01-10 16:20:46 +0100

    Refactored completion_validator.py

--- crazy_complete/completion_validator.py
+++ crazy_complete/completion_validator.py
@@ -2,73 +2,47 @@
 
 from .errors import CrazyError, CrazyTypeError
 
-def get_required_arg(args, name):
+def _get_required_arg(args, name):
     try:
         return args.pop(0)
     except IndexError:
         raise CrazyError(f'Missing argument: {name}') from None
 
-def get_optional_arg(args, default=None):
+def _get_optional_arg(args, default=None):
     try:
         return args.pop(0)
     except IndexError:
         return default
 
-def require_no_more(args):
+def _require_no_more(args):
     if args:
         raise CrazyError(f'Too many arguments: {args}')
 
-class CompletionValidator:
-    @staticmethod
-    def validate_complete(complete):
-        if complete:
-            complete = list(complete)
-            command  = complete.pop(0)
+def validate_complete(complete):
+    if not complete:
+        return
 
-            if not isinstance(command, str):
-                raise CrazyError(f"Command is not a string: {command}")
+    args = list(complete)
+    command = args.pop(0)
 
-            if not hasattr(CompletionValidator, command):
-                raise CrazyError(f"Unknown command for `complete`: {command}")
+    if not isinstance(command, str):
+        raise CrazyError(f"Command is not a string: {command}")
 
-            getattr(CompletionValidator, command)(complete)
+    NO_ARG_COMMANDS = [
+        "command", "group", "hostname", "pid", "process",
+        "user", "service", "signal", "variable", "environment"]
 
-    @staticmethod
-    def validate_commandline(cmdline):
-        for option in cmdline.get_options():
-            try:
-                CompletionValidator.validate_complete(option.complete)
-            except CrazyError as e:
-                raise CrazyError("%s: %s: %s" % (
-                    cmdline.get_command_path(),
-                    '|'.join(option.option_strings),
-                    e)) from e
+    if command == "none":
+        pass
 
-        for positional in cmdline.get_positionals():
-            try:
-                CompletionValidator.validate_complete(positional.complete)
-            except CrazyError as e:
-                raise CrazyError("%s: %d (%s): %s" % (
-                    cmdline.get_command_path(),
-                    positional.number,
-                    positional.metavar,
-                    e)) from e
+    elif command in NO_ARG_COMMANDS:
+        _require_no_more(args)
 
-    @staticmethod
-    def validate_commandlines(cmdline):
-        cmdline.visit_commandlines(lambda o: CompletionValidator.validate_commandline(o))
-
-    @staticmethod
-    def none(args):
-        return ()
-
-    @staticmethod
-    def choices(args):
-        choices = get_required_arg(args, 'values')
-        require_no_more(args)
+    elif command == 'choices':
+        choices = _get_required_arg(args, 'values')
+        _require_no_more(args)
 
         if hasattr(choices, 'items'):
-            new_choices = {}
             for item, desc in choices.items():
                 if not isinstance(item, (str, int, float)):
                     raise CrazyError(f'Item not a string/int/float: {item}')
@@ -76,35 +50,21 @@ class CompletionValidator:
                 if not isinstance(desc, (str, int, float)):
                     raise CrazyError(f'Description not a string/int/float: {desc}')
 
-                new_choices[str(item)] = str(desc)
-
-            return (new_choices,)
-
-        if isinstance(choices, (list, tuple)):
-            new_choices = []
+        elif isinstance(choices, (list, tuple)):
             for item in choices:
                 if not isinstance(item, (str, int, float)):
                     raise CrazyError(f'Item not a string/int/float: {item}')
 
-                new_choices.append(item)
-            return (new_choices,)
+        else:
+            raise CrazyError('values: Not a list or dictionary')
 
-        raise CrazyError('values: Not a list or dictionary')
-
-    @staticmethod
-    def command(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def directory(args):
-        opts = get_optional_arg(args, {})
-        require_no_more(args)
+    elif command in ['file', 'directory']:
+        opts = _get_optional_arg(args, {})
+        _require_no_more(args)
 
         if not isinstance(opts, dict):
             raise CrazyTypeError('options', 'dict', opts)
 
-        directory = None
         for key, value in opts.items():
             if key == 'directory':
                 if not isinstance(value, str):
@@ -112,62 +72,14 @@ class CompletionValidator:
 
                 if value == '':
                     raise CrazyError('directory: Cannot be empty')
-
-                directory = value
             else:
                 raise CrazyError(f'Unknown option: {key}')
 
-        return ({'directory': directory},)
-
-    @staticmethod
-    def file(args):
-        opts = get_optional_arg(args, {})
-        require_no_more(args)
-
-        if not isinstance(opts, dict):
-            raise CrazyTypeError('options', 'dict', opts)
-
-        directory = None
-        for key, value in opts.items():
-            if key == 'directory':
-                if not isinstance(value, str):
-                    raise CrazyError(f"directory: Not a string: {value}")
-
-                if value == '':
-                    raise CrazyError('directory: Cannot be empty')
-
-                directory = value
-            else:
-                raise CrazyError(f'Unknown option: {key}')
-
-        return ({'directory': directory},)
-
-    @staticmethod
-    def group(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def hostname(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def pid(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def process(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def range(args):
-        start = get_required_arg(args, "start")
-        stop  = get_required_arg(args, "stop")
-        step  = get_optional_arg(args, 1)
-        require_no_more(args)
+    elif command == 'range':
+        start = _get_required_arg(args, "start")
+        stop  = _get_required_arg(args, "stop")
+        step  = _get_optional_arg(args, 1)
+        _require_no_more(args)
 
         if not isinstance(start, int):
             raise CrazyError(f"start: not an int: {start}")
@@ -187,47 +99,16 @@ class CompletionValidator:
         else:
             raise CrazyError("step: cannot be 0")
 
-        return args
-
-    @staticmethod
-    def user(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def signal(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def service(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def variable(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def environment(args):
-        require_no_more(args)
-        return ()
-
-    @staticmethod
-    def exec(args):
-        cmd = get_required_arg(args, 'command')
-        require_no_more(args)
+    elif command == 'exec':
+        cmd = _get_required_arg(args, 'command')
+        _require_no_more(args)
 
         if not isinstance(cmd, str):
             raise CrazyError(f"Command is not a string: {cmd}")
 
-        return (cmd,)
-
-    @staticmethod
-    def value_list(args):
-        opts = get_required_arg(args, 'options')
-        require_no_more(args)
+    elif command == 'value_list':
+        opts = _get_required_arg(args, 'options')
+        _require_no_more(args)
 
         values = None
         separator = ','
@@ -267,4 +148,28 @@ class CompletionValidator:
         if len(separator) != 1:
             raise CrazyError(f'Invalid length for separator: {separator}')
 
-        return ({'values': values, 'separator': separator},)
+    else:
+        raise CrazyError(f"Unknown command for `complete`: {command}")
+
+def validate_commandline(cmdline):
+    for option in cmdline.get_options():
+        try:
+            validate_complete(option.complete)
+        except CrazyError as e:
+            raise CrazyError("%s: %s: %s" % (
+                cmdline.get_command_path(),
+                '|'.join(option.option_strings),
+                e)) from e
+
+    for positional in cmdline.get_positionals():
+        try:
+            validate_complete(positional.complete)
+        except CrazyError as e:
+            raise CrazyError("%s: %d (%s): %s" % (
+                cmdline.get_command_path(),
+                positional.number,
+                positional.metavar,
+                e)) from e
+
+def validate_commandlines(cmdline):
+    cmdline.visit_commandlines(lambda o: validate_commandline(o))
