6fc27b2658229fca4a08ff669183a42e7236a29c
Benjamin Abendroth <braph93@gmx.de>
2025-08-31 13:46:44 +0200

    Version bump to 0.3.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='crazy-complete',
-    version='0.3.3',
+    version='0.3.4',
     author='Benjamin Abendroth',
     author_email='braph93@gmx.de',
     packages=['crazy_complete'],
